4715464d91286780c4557eda963500ae35ad80e6
Rafael Silverio <fael_st@hotmail.com>
2021-02-25 01:40:44 -0300

    fish count down implements

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,21 +1,86 @@
+import { useEffect, useState } from "react";
 import styles from "../styles/components/CountDown.module.css";
 
+let countDownTimeout: NodeJS.Timeout;
+
 export function CountDown() {
+  const [time, setTime] = useState(0.05 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minutoFirst, minuteSecond] = String(minutes)
+    .padStart(2, "0")
+    .split("");
+  const [secondFirst, secondSecond] = String(seconds)
+    .padStart(2, "0")
+    .split("");
+
+  function startCountDown() {
+    setIsActive(true);
+  }
+  function resetCountDown() {
+    clearTimeout(countDownTimeout);
+    setIsActive(false);
+    setTime(25 * 60);
+  }
+
+  useEffect(() => {
+    if (isActive && time > 0) {
+      countDownTimeout = setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    } else if (isActive && time == 0) {
+      setHasFinished(true);
+      setIsActive(false);
+    }
+  }, [isActive, time]);
+
   return (
     <div>
       <div className={styles.containerCountDown}>
         <div>
-          <span>2</span>
-          <span>5</span>
+          <span>{minutoFirst}</span>
+          <span>{minuteSecond}</span>
         </div>
         <span>:</span>
         <div>
-          <span>0</span>
-          <span>0</span>
+          <span>{secondFirst}</span>
+          <span>{secondSecond}</span>
         </div>
       </div>
 
-      <button type="button" className={styles.btnStartCountDown}>Iniciar cotador</button>
+      {hasFinished ? (
+        <button
+          disabled
+          type="button"
+          className={`${styles.btnStartCountDown}`}
+        >
+          Cliclo encerrado
+        </button>
+      ) : (
+        <>
+          {!isActive ? (
+            <button
+              onClick={startCountDown}
+              type="button"
+              className={`${styles.btnStartCountDown}`}
+            >
+              Iniciar cotador
+            </button>
+          ) : (
+            <button
+              onClick={resetCountDown}
+              type="button"
+              className={`${styles.btnStartCountDown} ${styles.btnStartCountDownActive}`}
+            >
+              Cancelar ciclo
+            </button>
+          )}
+        </>
+      )}
     </div>
   );
 }
